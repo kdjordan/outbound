eee5e63a0f58dafe9b8f2eb3b46ec8c2f070cd64
Kevin Jordan <macdev2@Kevins-MacBook-Pro.local>
2020-04-10 21:29:10 -0700

    landing page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="app-container">
+      <img src="./assets/ob-logo.svg">
+      <h1>{{ msg }}</h1>
+    </div>
   </div>
 </template>
 
@@ -24,7 +12,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Coming Soon...'
     }
   }
 }
@@ -37,7 +25,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.app-container {
+    margin-top: 25%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
 }
 
 h1, h2 {
@@ -56,5 +52,10 @@ li {
 
 a {
   color: #42b983;
+}
+
+img {
+  width: 30%;
+
 }
 </style>
